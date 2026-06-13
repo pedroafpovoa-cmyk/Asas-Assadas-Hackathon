918ba04f124eaa83c3c95933a27e9e0431205843
Pedrin_MK <pedroafpovoa@gmail.com>
2026-06-13 10:59:38 -0300

    Create botoes.py

---
+++ fpsandexecucao.py
@@ -0,0 +1,16 @@
+import pygame
+import botoes
+
+pygame.init()
+largura = 800
+altura = 800
+tela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption("Capyvaras Powerfull Cotton Fight")
+clock = pygame.time.Clock()
+FPS = 60
+
+Executando = True
+while Executando:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            Executando = False
