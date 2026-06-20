8519978c8f6e9b4f7db7bc86af26bc4fa399a003
Pedrin_MK <pedroafpovoa@gmail.com>
2026-06-20 11:01:37 -0300

    Update capitais_paises.py

--- capitais_paises.py
+++ capitais_paises.py
@@ -1,43 +1,21 @@
-Capitais_Paises = {
-    "Lisboa": "Portugal",
-    "Madri": "Espanha",
-    "Paris": "França",
-    "Bruxelas": "Bélgica",
-    "Amsterdã": "Países Baixos",
-    "Luxemburgo": "Luxemburgo",
-    "Berlim": "Alemanha",
-    "Viena": "Áustria",
-    "Berna": "Suíça",
-    "Vaduz": "Liechtenstein",
-    "Mônaco": "Mônaco",
-    "Belgrado": "Sérvia",
-    "Zagreb": "Croácia",
-    "Liubliana": "Eslovênia",
-    "Sarajevo": "Bósnia e Herzegovina",
-    "Escópia": "Macedônia do Norte",
-    "Podgorica": "Montenegro"
-}
-
 dados = {
-    "Lisboa": "Portugal",
-    "Madri": "Espanha",
-    "Paris": "França",
-    "Bruxelas": "Bélgica",
-    "Amsterdã": "Países Baixos",
-    "Luxemburgo": "Luxemburgo",
-    "Berlim": "Alemanha",
-    "Viena": "Áustria",
-    "Berna": "Suíça",
-    "Vaduz": "Liechtenstein",
-    "Mônaco": "Mônaco",
-    "Belgrado": "Sérvia",
-    "Zagreb": "Croácia",
-    "Liubliana": "Eslovênia",
-    "Sarajevo": "Bósnia e Herzegovina",
-    "Escópia": "Macedônia do Norte",
-    "Podgorica": "Montenegro"
+    "Avestruz",
+    "Ema",
+    "Emu",
+    "Casuar",
+    "Kiwi",
+    "Pinguim",
+    "Cormorão-das-Galápagos",
+    "Kakapo",
+    "Takahe",
+    "Weka",
+    "Trilho",
+    "Frango-d'água",
+    "Mergulhão",
+    "Pato-vapor",
+    "Marreca"
 }
 
-with open("capitais.txt", "w", encoding="utf-8") as arquivo:
-    for capital, pais in dados.items():
-        arquivo.write(f"{capital}: {pais}\n")
+with open("aves.txt", "w", encoding="utf-8") as arquivo:
+    for ave in dados:
+        arquivo.write(f"{ave}\n")
