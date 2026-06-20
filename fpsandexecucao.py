b752afa89ebb41b43d1ac19aba2a51d454bf5f29
Pedrin_MK <pedroafpovoa@gmail.com>
2026-06-20 11:01:33 -0300

    Update fpsandexecucao.py

--- fpsandexecucao.py
+++ fpsandexecucao.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import pygame
 import botoes
 
@@ -13,4 +14,21 @@ Executando = True
 while Executando:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
+=======
+import pygame
+import botoes
+
+pygame.init()
+largura = 800
+altura = 800
+tela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption("Capyvaras Powerfull Cotton Fight")
+clock = pygame.time.Clock()
+FPS = 60
+
+Executando = True
+while Executando:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+>>>>>>> 52d371d3c8fccc6fba9ce2832645e2bd4a2e0e46
             Executando = False
