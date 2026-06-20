ddd754274065e315287191db20075968b7fb803e
Pedrin_MK <pedroafpovoa@gmail.com>
2026-06-20 11:01:27 -0300

    Update musica.py

--- musica.py
+++ musica.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import pygame
 
 import Tela_inicial
@@ -15,4 +16,21 @@ while Executando:
         if evento.type == pygame.QUIT:
             Executando = False
 
+=======
+import pygame
+
+pygame.init()
+pygame.mixer.init()
+
+pygame.mixer.music.load("In The End (Linkin Park).mp3")
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play(-1)
+
+Executando = True
+while Executando:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            Executando = False
+
+>>>>>>> 52d371d3c8fccc6fba9ce2832645e2bd4a2e0e46
 pygame.quit()
