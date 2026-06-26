a87861f4438cd448aae0e8ec726c439993c1155b
agnes dos reis <pedroafpovoa@gmail.com>
2026-06-26 13:51:36 -0300

    Update Tela_inicial.py

--- Tela_inicial.py
+++ Tela_inicial.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import pygame
 
 import botoes
@@ -24,3 +25,31 @@ while Executando:
             Executando = False
 
 pygame.quit()
+=======
+import pygame
+
+import botoes
+
+import musica
+
+pygame.init()
+largura = 800
+altura = 800
+tela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption("Hack Tasks")
+clock = pygame.time.Clock()
+FPS = 60
+
+imagem_fundo = pygame.image.load("Tela_menu.png.png").convert()
+imagem_fundo = pygame.transform.scale(imagem_fundo, (largura, altura))
+tela.blit(imagem_fundo, (0, 0))
+pygame.display.flip()
+
+Executando = True
+while Executando:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            Executando = False
+pygame.quit()
+#py -3.12 Tela_inicial.py 
+>>>>>>> Stashed changes
